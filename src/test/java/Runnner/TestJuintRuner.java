5875dc4b0d85ee1318bc3adfc9164a49799ee569
meerakop <prasadu.testing@gmail.com>
2022-12-25 08:55:11 +0530

    Updated
    Updated

---
+++ src/test/java/Runnner/TestJuintRuner.java
@@ -0,0 +1,13 @@
+package Runnner;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestJuintRuner {
+
+	@Test
+    public void testLucky() {
+		System.out.println("-------------------------------------------------->");
+        Assert.assertEquals("7", "7");
+    }
+}
